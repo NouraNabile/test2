97b1475ec0c31fb0712edf9c028e74115afea07f
NouraNabile <123950714+NouraNabile@users.noreply.github.com>
2023-03-09 21:28:27 +0200

    Create mainControl.py

---
+++ mainControl.py
@@ -0,0 +1,51 @@
+from Book import Book
+from DatabaseLibrary import MainDataBase
+from Librarian import Lib_User
+from NormalUser import Normal_User
+
+
+
+# p1 = Lib_User("mohamed", "mohamed@gmail.com", "2554")
+# p2 = Lib_User("mohamed2", "mohamed@gmail.com", "2554")
+# p3 = Lib_User("mohamed3", "mohamed@gmail.com", "2554")
+# p4 = Normal_User("mohamed4", "mohamed@gmail.com", "2554")
+
+# p1.set_list_user(p2)
+# p1.set_list_user(p3)
+# p1.set_list_user(p4)
+
+# book1 = Book('alice in wonderland', 'Lewis Carroll', p1.ID)
+
+# book2 = Book('Oliver Twist', 'charles dickens', p2.ID)
+# book3 = Book('Oliver Twist', 'charles dickens', p3.ID)
+
+# MainDataBase.set_list_available_book(book1)
+# MainDataBase.set_list_available_book(book2)
+# MainDataBase.set_list_available_book(book3)
+# # book2.extendLoanPeriod(8)  # enter num of days
+# # MainDataBase.get_list_available_book()
+# # MainDataBase.get_numOfAvailable(book3)
+# ################################
+# # MainDataBase.set_list_borrowed_book(book1,p1.ID)
+# # MainDataBase.set_list_borrowed_book(book2,p2.ID)
+# # MainDataBase.set_list_borrowed_book(book3,p3.ID)
+
+# # MainDataBase.get_numOfAvailable(book3)
+# # MainDataBase.get_list_borrowed_book()
+# #################################
+
+# # p1.searchFor_book(book3)
+# # book4 = Book("Naroto", "kim", "4")
+# # p1.Add_Book(book4)
+# # MainDataBase.get_list_available_book()
+
+# # p1.Delete_Book(book3)
+# # p1.get_list_user()
+
+# MainDataBase.get_list_available_book()
+# p4.requestFor_borrowed_book(book3, 3, p4.ID)
+# p4.ExtendLoanBook(book3, 10)
+# # p4.ExtendLoanBook(book3, 10)
+# # MainDataBase.get_list_available_book()
+# p4.requestFor_return_book(book3, 2)
+# MainDataBase.get_list_borrowed_book()
